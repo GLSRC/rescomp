769582db7e9e28ba3f4f900bb398355016f96ac0
Sebastian Baur <sebastian.baur@dlr.de>
2019-11-22 16:15:49 +0100

    formatting
    Former-commit-id: bfbbd853971c44ca2bca45e1528c1c345fea8f21

--- measures.py
+++ measures.py
@@ -14,14 +14,15 @@ import scipy.spatial
 
 from . import esn_rescomp
 
+
 class reservoir(esn_rescomp.res_core):
     def __init__(self, **kwargs):
-        self.demerge_time = None #demerge_time() assigns value
-        self.lyapunov = None #max_lyapunov_nn() assigns value
+        self.demerge_time = None  # demerge_time() assigns value
+        self.lyapunov = None  # max_lyapunov_nn() assigns value
         self.lyapunov_test = None
-        self.dimension = None #correlation_dimension(traj) assigns value
+        self.dimension = None  # correlation_dimension(traj) assigns value
         self.dimension_test = None
-        #self.chi = None #calc_chi
+        # self.chi = None #calc_chi
         self.in_strength = None
         self.avg_in_strength = None
         self.out_strength = None
@@ -39,13 +40,13 @@ class reservoir(esn_rescomp.res_core):
         """
         if flag == 'train':
             error = np.sqrt(((np.matmul(self.W_out, self.r.T) - \
-                                        self.y_train.T)**2).sum() \
-                    / (self.y_train**2).sum())
+                              self.y_train.T) ** 2).sum() \
+                            / (self.y_train ** 2).sum())
             self.RMSE_train = error
         elif flag == 'pred':
             error = np.sqrt(((np.matmul(self.W_out, self.r_pred.T) - \
-                                        self.y_test.T)**2).sum() \
-                    / (self.y_test**2).sum())
+                              self.y_test.T) ** 2).sum() \
+                            / (self.y_test ** 2).sum())
             self.RMSE_pred = error
         else:
             raise Exception('use "train" or "pred" as flag')
@@ -75,6 +76,7 @@ class reservoir(esn_rescomp.res_core):
                 continue
             break
         """
+
     def return_map(self, axis=2):
         """
         Shows the recurrence plot of the maxima of a given axis
@@ -91,7 +93,7 @@ class reservoir(esn_rescomp.res_core):
         plt.show()
 
     def calc_dimension(self, r_min=0.5, r_max=5., r_steps=0.15,
-                              plot=False, test_measure=False):
+                       plot=False, test_measure=False):
         """
         Calculates correlation dimension for self.y_pred (or self.y_test) using
         the algorithm by Grassberger and Procaccia and stores it in
@@ -109,18 +111,18 @@ class reservoir(esn_rescomp.res_core):
             N_r(radius) ~ radius**dimension
         """
         if test_measure:
-            traj = self.y_test # for measure assessing
+            traj = self.y_test  # for measure assessing
         else:
-            traj = self.y_pred # for evaluating prediction
+            traj = self.y_pred  # for evaluating prediction
 
         nr_points = float(traj.shape[0])
         radii = np.arange(r_min, r_max, r_steps)
 
         tree = scipy.spatial.cKDTree(traj)
-        N_r = np.array(tree.count_neighbors(tree, radii), dtype=float)/ nr_points
+        N_r = np.array(tree.count_neighbors(tree, radii), dtype=float) / nr_points
         N_r = np.vstack((radii, N_r))
 
-        #linear fit based on loglog scale, to get slope/dimension:
+        # linear fit based on loglog scale, to get slope/dimension:
         slope, intercept = np.polyfit(np.log(N_r[0]), np.log(N_r[1]), deg=1)[0:2]
         if test_measure:
             self.dimension_test = slope
@@ -130,11 +132,11 @@ class reservoir(esn_rescomp.res_core):
         ###plotting
         if plot:
             plt.loglog(N_r[0], N_r[1], 'x', basex=10., basey=10.)
-            plt.title('loglog plot of the N_r(radius), slope/dim = '+ str(slope))
+            plt.title('loglog plot of the N_r(radius), slope/dim = ' + str(slope))
             plt.show()
 
     def calc_lyapunov(self, threshold=int(10),
-                        plot=False, print_switch=False, test_measure=False):
+                      plot=False, print_switch=False, test_measure=False):
         """
         Calculates the maximal Lyapunov Exponent of self.y_pred (or self.y_test),
         by estimating the time derivative of the mean logarithmic distances of
@@ -158,58 +160,56 @@ class reservoir(esn_rescomp.res_core):
         t0 = time.time()
 
         ###Definition of taus:        
-        tau_min=int(0.5/self.dt)
-        tau_max=int(3.8/self.dt)
-        taus = np.arange(tau_min, tau_max, 10) #taus = np.array([tau_min, tau_max])
+        tau_min = int(0.5 / self.dt)
+        tau_max = int(3.8 / self.dt)
+        taus = np.arange(tau_min, tau_max, 10)  # taus = np.array([tau_min, tau_max])
 
         if test_measure:
-            traj = self.y_test # for measure assessing
+            traj = self.y_test  # for measure assessing
         else:
-            traj = self.y_pred # for evaluating prediction
-
+            traj = self.y_pred  # for evaluating prediction
 
         tree = scipy.spatial.cKDTree(traj)
         self.nn_index = tree.query(traj, k=2)[1]
 
-        #drop all elements in nn_index lists where the neighbour is:
-        #1. less than 200 timesteps away
-        #2. where we cannot calculate the neighbours future in tau_max timesteps:
+        # drop all elements in nn_index lists where the neighbour is:
+        # 1. less than 200 timesteps away
+        # 2. where we cannot calculate the neighbours future in tau_max timesteps:
 
-        #contains indices of points and the indices of their nn:
+        # contains indices of points and the indices of their nn:
 
-        self.nn_index = self.nn_index[np.abs(self.nn_index[:,0] - self.nn_index[:,1]) > threshold]
+        self.nn_index = self.nn_index[np.abs(self.nn_index[:, 0] - self.nn_index[:, 1]) > threshold]
 
-        self.nn_index = self.nn_index[self.nn_index[:,1] + tau_max < traj.shape[0]]
-        self.nn_index = self.nn_index[self.nn_index[:,0] + tau_max < traj.shape[0]]
+        self.nn_index = self.nn_index[self.nn_index[:, 1] + tau_max < traj.shape[0]]
+        self.nn_index = self.nn_index[self.nn_index[:, 0] + tau_max < traj.shape[0]]
 
-        #Calculate the largest Lyapunov exponent:
-        #for storing the results:
+        # Calculate the largest Lyapunov exponent:
+        # for storing the results:
         Sum = []
-        #loop over differnt tau, to get a functional dependence:
+        # loop over differnt tau, to get a functional dependence:
         for tau in taus:
-            #print(tau)
+            # print(tau)
 
-            S = []#the summed values for all basis points
+            S = []  # the summed values for all basis points
 
-            #loop over every point in the trajectory, where we can calclutate
-            #the future in tau_max timesteps:
+            # loop over every point in the trajectory, where we can calclutate
+            # the future in tau_max timesteps:
             for point, nn in self.nn_index:
+                S.append(np.log(np.linalg.norm(traj[point + tau] - traj[nn + tau])))  # add one points average s to S
 
-                S.append(np.log(np.linalg.norm(traj[point+tau] - traj[nn + tau]))) #add one points average s to S
-
-                #since there is no else, we only avg over the points that have
-                #points in their epsilon environment
-            Sum.append((tau*self.dt, np.array(S).mean()))
+                # since there is no else, we only avg over the points that have
+                # points in their epsilon environment
+            Sum.append((tau * self.dt, np.array(S).mean()))
         self.Sum = np.array(Sum)
 
-        slope = (self.Sum[-1,1] - self.Sum[0,1]) / (self.Sum[-1,0] - self.Sum[0,0])
+        slope = (self.Sum[-1, 1] - self.Sum[0, 1]) / (self.Sum[-1, 0] - self.Sum[0, 0])
         if plot:
             plt.title('slope: ' + str(slope))
-            plt.plot(self.Sum[:,0], self.Sum[:,1])
-            plt.plot(self.Sum[:,0], self.Sum[:,0]*slope)
+            plt.plot(self.Sum[:, 0], self.Sum[:, 1])
+            plt.plot(self.Sum[:, 0], self.Sum[:, 0] * slope)
             plt.xlabel('time dt*tau[steps]')
             plt.ylabel('log_dist_former_neighbours')
-            #plt.plot(self.Sum[:,0], self.Sum[:,0]*slope + self.Sum[0,0])
+            # plt.plot(self.Sum[:,0], self.Sum[:,0]*slope + self.Sum[0,0])
             plt.show()
 
         if test_measure:
@@ -222,9 +222,9 @@ class reservoir(esn_rescomp.res_core):
         Shows a histogram of the fitted parameters of self.W_out, each output
         dimension in an other color
         """
-        f = plt.figure(figsize = (10,10))
+        f = plt.figure(figsize=(10, 10))
         for i in np.arange(self.ydim):
-            plt.hist(self.W_out[i], bins=30, alpha=0.5, label='W_out['+str(i)+']')
+            plt.hist(self.W_out[i], bins=30, alpha=0.5, label='W_out[' + str(i) + ']')
         plt.legend(fontsize=10)
         f.show()
 
@@ -247,7 +247,7 @@ class reservoir(esn_rescomp.res_core):
         """
         network = self.binary_network
         k = network.sum(axis=0)
-        C = np.diag(np.matmul(np.matmul(network, network), network))/ k*(k-1)
+        C = np.diag(np.matmul(np.matmul(network, network), network)) / k * (k - 1)
         self.clustering_coeff = C
         self.avg_clustering_coeff = C.mean()
 
@@ -259,32 +259,32 @@ class reservoir(esn_rescomp.res_core):
         't': the x,y plot of the trajectory
         """
         ### plotting parameter in dict
-        train_dict = { 'color': 'green', 'alpha': 0.2, 'label': 'train'}
-        pred_dict = { 'color': 'red', 'alpha': 0.4, 'label': 'pred'}
+        train_dict = {'color': 'green', 'alpha': 0.2, 'label': 'train'}
+        pred_dict = {'color': 'red', 'alpha': 0.4, 'label': 'pred'}
 
         if flag == 't':
-            f = plt.figure(figsize = (15,15))
+            f = plt.figure(figsize=(15, 15))
             ax1 = f.add_subplot(111)
 
             ax1.set_title('x,y plot of prediction')
-            ax1.plot(self.y_pred[:,0], self.y_pred[:,1], **pred_dict)
-            ax1.plot(self.y_test[:,0], self.y_test[:,1], **train_dict)
+            ax1.plot(self.y_pred[:, 0], self.y_pred[:, 1], **pred_dict)
+            ax1.plot(self.y_test[:, 0], self.y_test[:, 1], **train_dict)
             ax1.legend()
 
         elif flag == 'r':
             split = 0.1
-            f = plt.figure(figsize = (15,15))
+            f = plt.figure(figsize=(15, 15))
             ax1 = f.add_subplot(121)
             ax1.set_ylim((-1.05, 1.05))
             ax1.set_title('network during training')
             ax1.plot(np.arange(self.r.shape[0]),
-                     self.r[:,self.calc_tt(flag='bool_1d', split=split)])
+                     self.r[:, self.calc_tt(flag='bool_1d', split=split)])
 
             ax2 = f.add_subplot(122)
             ax2.set_ylim((-1.05, 1.05))
             ax2.set_title('network during prediction')
             ax2.plot(np.arange(self.r_pred.shape[0]),
-                     self.r_pred[:,self.calc_tt(flag='bool_1d', split=split)])
+                     self.r_pred[:, self.calc_tt(flag='bool_1d', split=split)])
 
         elif flag == 'y':
             # f = plt.figure(figsize = (15,15))
@@ -296,26 +296,26 @@ class reservoir(esn_rescomp.res_core):
                 self.training_steps + self.prediction_steps)
             time_range_train = np.arange(self.training_steps)
 
-            #y-values
+            # y-values
             if self.sys_flag == "roessler":
-                ylimits = ((-5,8), (-8,5), (0, 15))
+                ylimits = ((-5, 8), (-8, 5), (0, 15))
             elif self.sys_flag == 'mod_lorenz':
-                ylimits = ((-15,20), (-15,30), (-10,50))
+                ylimits = ((-15, 20), (-15, 30), (-10, 50))
             else:
                 traj = np.concatenate((self.y_test, self.y_train), axis=0)
-                ylimits = [[traj[:,dim].min(), traj[:,dim].max()] for dim in np.arange(self.ydim)]
+                ylimits = [[traj[:, dim].min(), traj[:, dim].max()] for dim in np.arange(self.ydim)]
             for i, ylim in enumerate(ylimits):
                 ax1 = axes[i][0]
                 ax1.set_ylim(ylim)
-                ax1.set_title('y['+str(i)+']_value_train')
-                ax1.plot(time_range_train, self.y_train[:,i], **train_dict)
+                ax1.set_title('y[' + str(i) + ']_value_train')
+                ax1.plot(time_range_train, self.y_train[:, i], **train_dict)
 
                 # ax2 = plt.subplot(self.ydim,2,2*i+2)
-                ax2= axes[i][1]
-                ax2.set_title('y['+str(i)+']_value_pred')
+                ax2 = axes[i][1]
+                ax2.set_title('y[' + str(i) + ']_value_pred')
                 ax2.set_ylim(ylim)
-                ax2.plot(time_range_pred[:], self.y_pred[:,i], **pred_dict)
-                ax2.plot(time_range_pred[:], self.y_test[:,i], **train_dict)
+                ax2.plot(time_range_pred[:], self.y_pred[:, i], **pred_dict)
+                ax2.plot(time_range_pred[:], self.y_test[:, i], **train_dict)
                 ax2.legend()
 
         else:
@@ -339,12 +339,12 @@ class reservoir(esn_rescomp.res_core):
         Stores it in self.weighted_cc and its mean in self.avg_weighted_cc.
         """
         k = self.binary_network.sum(axis=0)
-        #print(k)
-        network = abs(self.network)/ abs(self.network).max()
+        # print(k)
+        network = abs(self.network) / abs(self.network).max()
 
         self.weighted_cc = np.diag(np.matmul(np.cbrt(network),
-                                 np.matmul(np.cbrt(network), np.cbrt(network)))) \
-               / (k*(k-1))
+                            np.matmul(np.cbrt(network), np.cbrt(network)))) \
+                           / (k * (k - 1))
         # assign 0. to infinit values:
         self.weighted_cc[np.isnan(self.weighted_cc)] = 0.
 
@@ -383,30 +383,30 @@ class reservoir(esn_rescomp.res_core):
             raise Exception('values in split not between -1. and 1., type: ', type(split))
 
         remaining_size = sum(np.abs(split))
-        new = reservoir(sys_flag=self.sys_flag, N=int(round(self.N*(1-remaining_size))),
-                      input_dimension=3, output_dimension=3,
-                      type_of_network=self.type, dt = self.dt,
-                      training_steps=self.training_steps,
-                      prediction_steps=self.prediction_steps,
-                      discard_steps=self.discard_steps,
-                      regularization_parameter=self.reg_param,
-                      spectral_radius=self.spectral_radius,
-                      input_weight=self.input_weight, avg_degree=self.avg_degree,
-                      epsilon=self.epsilon, extended_states=self.extended_states,
-                      W_in_sparse=self.W_in_sparse, W_in_scale=self.W_in_scale)
-        #gather to be removed nodes arguments in rm_args:
+        new = reservoir(sys_flag=self.sys_flag, N=int(round(self.N * (1 - remaining_size))),
+                        input_dimension=3, output_dimension=3,
+                        type_of_network=self.type, dt=self.dt,
+                        training_steps=self.training_steps,
+                        prediction_steps=self.prediction_steps,
+                        discard_steps=self.discard_steps,
+                        regularization_parameter=self.reg_param,
+                        spectral_radius=self.spectral_radius,
+                        input_weight=self.input_weight, avg_degree=self.avg_degree,
+                        epsilon=self.epsilon, extended_states=self.extended_states,
+                        W_in_sparse=self.W_in_sparse, W_in_scale=self.W_in_scale)
+        # gather to be removed nodes arguments in rm_args:
         rm_args = np.empty(0)
         for s in split:
             rm_args = np.append(self.calc_tt(flag='arg', split=s), rm_args)
-            #print(s, rm_args.shape)
+            # print(s, rm_args.shape)
 
-        #rows and columns of network are deleted according to rm_args:
+        # rows and columns of network are deleted according to rm_args:
         new.network = np.delete(np.delete(self.network, rm_args, 0), rm_args, 1)
 
-        #the new average degree is calculated:
+        # the new average degree is calculated:
         new.calc_binary_network()
         new.avg_degree = new.binary_network.sum(axis=0).mean(axis=0)
-        #the new spectral radius is calculated:
+        # the new spectral radius is calculated:
         new.network = scipy.sparse.csr_matrix(new.network)
 
         eigenvals = scipy.sparse.linalg.eigs(new.network, k=1, which='LM')[0]
@@ -419,7 +419,7 @@ class reservoir(esn_rescomp.res_core):
 
         new.network = new.network.toarray()
 
-        #Adjust W_in
+        # Adjust W_in
         new.W_in = np.delete(self.W_in, rm_args, 0)
 
         return new

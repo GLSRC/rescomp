f6fba06c965a7f6c7d9d91c634507976b5c156fe
Baur, Sebastian <sebastian.baur@dlr.de>
2020-03-30 14:11:19 +0200

    Updated the version mismatch warning to also print the internal and environment versions
    Former-commit-id: f707144114dcf6b8d5296664762e955d2e04f5f9

--- rescomp/__init__.py
+++ rescomp/__init__.py
@@ -7,11 +7,15 @@ from .utilities import read_pickle
 from ._version import __version__
 
 if not utilities._compare_version_file_vs_env(segment_threshold="minor"):
+    int_version = utilities._get_internal_version()
+    env_version = utilities._get_environment_version()
+
     warn_string = \
-        "The internal rescomp package version number '%s' does not match the " \
-        "version number specified in the currently active python environment " \
+        "The internal rescomp package version '%s' does not match the " \
+        "version specified in the currently active python environment " \
         "'%s'.\nPlease update the package by entering the rescomp repository " \
-        "folder and running: 'pip install --upgrade -e .'"
+        "folder and running: 'pip install --upgrade -e .'"\
+        %(int_version, env_version)
 
     import warnings
     warnings.filterwarnings("once", category=ImportWarning)

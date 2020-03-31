ded5c3151ccd7e291d0ff6b0a175bedab26d6c57
Baur, Sebastian <sebastian.baur@dlr.de>
2020-03-31 17:35:05 +0200

    Added Roessler system to the simulations example
    Former-commit-id: f51f3bfb679e1c202cbf980ccc045ce4458143a7

--- bin/simulations_example.py
+++ bin/simulations_example.py
@@ -11,35 +11,58 @@ from mpl_toolkits import mplot3d
 simulation_time_steps = 1000
 starting_point = np.array([-14.03020521, -20.88693127, 25.53545])
 
-data_lorenz = rescomp.simulate_trajectory(
+sim_data = rescomp.simulate_trajectory(
     sys_flag='lorenz', dt=2e-2, time_steps=simulation_time_steps,
     starting_point=starting_point)
+
+
+# --- Plot the normal Lorenz 63 System
+
+fig = plt.figure(figsize=(6, 6))
+ax = fig.add_subplot(111, projection='3d')
+ax.plot(sim_data[:, 0], sim_data[:, 1], sim_data[:, 2],
+        alpha=0.8, color='blue', label='normal Lorenz63 system')
+plt.legend()
+plt.show()
 
 
 # --- Simulate the Lorenz 63 System with parameters different than the default
 sigma=20  # default: sigma=10
 rho=14  # default: rho=28
 b=8/3  # default: b=8/3
+# For more information on the possible parameters, please see the HTML
+# documentation on the simulate_trajectory function
 
-data_lorenz_diff = rescomp.simulate_trajectory(
+sim_data = rescomp.simulate_trajectory(
     sys_flag='lorenz', dt=2e-2, time_steps=simulation_time_steps,
     starting_point=starting_point, sigma=sigma, rho=rho, b=b)
 
-# --- Plot the normal Lorenz 63 System
-
-
-fig = plt.figure(figsize=(6, 6))
-ax = fig.add_subplot(111, projection='3d')
-ax.plot(data_lorenz[:, 0], data_lorenz[:, 1], data_lorenz[:, 2],
-        alpha=0.8, color='blue', label='normal Lorenz63 system')
-plt.legend()
-plt.show()
 
 # --- Plot the Lorenz 63 with different parameters
 
 fig = plt.figure(figsize=(6, 6))
 ax = fig.add_subplot(111, projection='3d')
-ax.plot(data_lorenz_diff[:, 0], data_lorenz_diff[:, 1], data_lorenz_diff[:, 2],
-        alpha=0.8, color='orange', label='Lorenz63 with different parameters')
+ax.plot(sim_data[:, 0], sim_data[:, 1], sim_data[:, 2],
+        alpha=0.8, color='blue', label='Lorenz63 with different parameters')
 plt.legend()
 plt.show()
+
+
+# --- To simulate the Roessler System instead, just change the sys_flag parameter
+
+simulation_time_steps = 5000
+starting_point = np.array([0, 0, 0])
+
+sim_data = rescomp.simulate_trajectory(
+    sys_flag='roessler', dt=2e-2, time_steps=simulation_time_steps,
+    starting_point=starting_point)
+
+# --- Plot the Roessler System
+
+fig = plt.figure(figsize=(6, 6))
+ax = fig.add_subplot(111, projection='3d')
+ax.plot(sim_data[:, 0], sim_data[:, 1], sim_data[:, 2],
+        alpha=0.8, color='blue', label='Roessler System')
+plt.legend()
+plt.show()
+

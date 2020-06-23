26315ac665ec86e7d69202dc950a7f11fc97cdb5
Baur, Sebastian <sebastian.baur@dlr.de>
2020-06-24 01:39:36 +0200

    Fixed a CRITICAL bug in the KS simulation
    Former-commit-id: 4d58f2138488f94a129f5fc5c1018877ab6329e2

--- rescomp/simulations.py
+++ rescomp/simulations.py
@@ -355,6 +355,7 @@ _sys_flag_synonyms.add_synonyms(10, "rabinovich")
 _sys_flag_synonyms.add_synonyms(11, "thomas")
 _sys_flag_synonyms.add_synonyms(12, "roessler_sprott")
 _sys_flag_synonyms.add_synonyms(13, "kuramoto_sivashinsky")
+_sys_flag_synonyms.add_synonyms(14, "kuramoto_sivashinsky_old")
 
 
 def simulate_trajectory(sys_flag='mod_lorenz', dt=2e-2, time_steps=int(2e4),
@@ -457,8 +458,15 @@ def simulate_trajectory(sys_flag='mod_lorenz', dt=2e-2, time_steps=int(2e4),
     elif sys_flag_syn == 12:
         f = lambda x: _roessler_sprott(x, **kwargs)
     elif sys_flag_syn == 13:
-        # Starting point is ignored here atm
+        # TODO Starting point is ignored here atm
+        if np.array_equal(starting_point, np.array([1, 2, 3])):
+            print("WARNING starting point is ignored for this simulation fct!")
         return _kuramoto_sivashinsky(dt=dt, time_steps=time_steps - 1, **kwargs)
+    elif sys_flag_syn == 14:
+        # TODO Starting point is ignored here atm
+        if np.array_equal(starting_point, np.array([1, 2, 3])):
+            print("WARNING starting point is ignored for this simulation fct!")
+        return _kuramoto_sivashinsky_old(dt=dt, time_steps=time_steps - 1, **kwargs)
     else:
         raise Exception('sys_flag not recoginized')
 
@@ -474,12 +482,14 @@ def simulate_trajectory(sys_flag='mod_lorenz', dt=2e-2, time_steps=int(2e4),
     return traj
 
 
-# TODO: Refactor to make more legible, then add a docstring, remove/add print statements etc.
-def _kuramoto_sivashinsky(dimensions, system_size, dt, time_steps):
-    """ This function simulates the Kuramoto–Sivashinsky PDE
+def _kuramoto_sivashinsky_old(dimensions, system_size, dt, time_steps):
+    """ This function INCORRECTLY simulates the Kuramoto–Sivashinsky PDE
 
-     Even though it doesn't use the RK4 algorithm, it is bundled with the other
-     simulation functions in simulate_trajectory() for consistency.
+    It is kept here only for historical reasons.
+    DO NOT USE UNLESS YOU WANT INCORRECT RESULTS
+
+    Even though it doesn't use the RK4 algorithm, it is bundled with the other
+    simulation functions in simulate_trajectory() for consistency.
 
     Reference for the numerical integration:
     "fourth order time stepping for stiff pde-kassam trefethen 2005" at
@@ -498,7 +508,7 @@ def _kuramoto_sivashinsky(dimensions, system_size, dt, time_steps):
 
     """
     n = dimensions  # No. of grid points in real space (and hence dimensionality of the output)
-    size = system_size  # grid points for the PDE simulation
+    size = system_size  #
 
     # Define initial conditions and Fourier Transform them
     x = np.transpose(np.conj(np.arange(1, n + 1))) / n
@@ -516,6 +526,7 @@ def _kuramoto_sivashinsky(dimensions, system_size, dt, time_steps):
     E = np.exp(h * L)
     E_2 = np.exp(h * L / 2)
     M = 16
+    # M = (size * np.pi) //2
     r = np.exp(1j * np.pi * (np.arange(1, M + 1) - 0.5) / M)
     LR = h * np.transpose(np.repeat([L], M, axis=0)) + np.repeat([r], n, axis=0)
     Q = h * np.real(np.mean((np.exp(LR / 2) - 1) / LR, axis=1))
@@ -524,6 +535,87 @@ def _kuramoto_sivashinsky(dimensions, system_size, dt, time_steps):
     f3 = h * np.real(np.mean((-4 - 3 * LR - LR ** 2 + np.exp(LR) * (4 - LR)) / LR ** 3, axis=1))
 
     uu = [np.array(u)]  # List of Real space solutions, later converted to a np.array
+
+    g = -0.5j * k  # TODO: Meaning?
+
+    # See paper for details
+    for n in range(1, nmax + 1):
+        Nv = g * np.fft.fft(np.real(np.fft.ifft(v)) ** 2)
+        a = E_2 * v + Q * Nv
+        Na = g * np.fft.fft(np.real(np.fft.ifft(a)) ** 2)
+        b = E_2 * v + Q * Na
+        Nb = g * np.fft.fft(np.real(np.fft.ifft(b)) ** 2)
+        c = E_2 * a + Q * (2 * Nb - Nv)
+        Nc = g * np.fft.fft(np.real(np.fft.ifft(c)) ** 2)
+
+        v = E * v + Nv * f1 + 2 * (Na + Nb) * f2 + Nc * f3
+        u = np.real(np.fft.ifft(v))
+        uu.append(np.array(u))
+
+    uu = np.array(uu)
+    # print("PDE simulation finished")
+
+    return uu
+
+# reproduce_ETDRK4_Kassam2005_KS_simulation
+def _kuramoto_sivashinsky(dimensions, system_size, dt, time_steps):
+    """ This function simulates the Kuramoto–Sivashinsky PDE
+
+    Even though it doesn't use the RK4 algorithm, it is bundled with the other
+    simulation functions in simulate_trajectory() for consistency.
+
+    Reference for the numerical integration:
+    "fourth order time stepping for stiff pde-kassam trefethen 2005" at
+    https://people.maths.ox.ac.uk/trefethen/publication/PDF/2005_111.pdf
+
+    Python implementation at: https://github.com/E-Renshaw/kuramoto-sivashinsky
+
+    Args:
+        dimensions (int): nr. of dimensions of the system grid
+        system_size (int): physical size of the system
+        dt (float): time step size
+        time_steps (int): nr. of time steps to simulate
+
+    Returns:
+        (np.ndarray): simulated trajectory of shape (time_steps, dimensions)
+
+    """
+    n = dimensions  # No. of grid points in real space (and hence dimensionality of the output)
+    size = system_size  # system size
+
+    # Define initial conditions and Fourier Transform them
+    x = size * np.transpose(np.conj(np.arange(1, n + 1))) / n
+    u = np.cos(2 * np.pi * x / size) * (1 + np.sin(2 * np.pi * x / size))
+    v = np.fft.fft(u)
+
+    h = dt  # time step
+    nmax = time_steps  # No. of time steps to simulate
+
+    # Wave numbers
+    k = np.transpose(
+        np.conj(np.concatenate((np.arange(0, n / 2), np.array([0]),
+                                np.arange(-n / 2 + 1, 0))))) * 2 * np.pi / size
+
+    # Just copied from the paper, it works
+    L = k ** 2 - k ** 4
+    E = np.exp(h * L)
+    E_2 = np.exp(h * L / 2)
+    M = int(size/(2 * np.pi))
+    # M = (size * np.pi) // 2
+    r = np.exp(1j * np.pi * (np.arange(1, M + 1) - 0.5) / M)
+    LR = h * np.transpose(np.repeat([L], M, axis=0)) + np.repeat([r], n, axis=0)
+    Q = h * np.real(np.mean((np.exp(LR / 2) - 1) / LR, axis=1))
+    f1 = h * np.real(
+        np.mean((-4 - LR + np.exp(LR) * (4 - 3 * LR + LR ** 2)) / LR ** 3,
+                axis=1))
+    f2 = h * np.real(
+        np.mean((2 + LR + np.exp(LR) * (-2 + LR)) / LR ** 3, axis=1))
+    f3 = h * np.real(
+        np.mean((-4 - 3 * LR - LR ** 2 + np.exp(LR) * (4 - LR)) / LR ** 3,
+                axis=1))
+
+    uu = [np.array(
+        u)]  # List of Real space solutions, later converted to a np.array
 
     g = -0.5j * k  # TODO: Meaning?
 

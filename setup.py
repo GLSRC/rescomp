405182315ad71731bf333262195f79fd76c0fc82
baurse <baurse@protonmail.com>
2021-11-17 18:14:19 +0100

    Slightly cleaned up setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ from rescomp._version import __version__
 
 setup(
     name='rescomp',
-    # version='0.0.2',
     version=__version__,
     description='Reservoir Computing package developed at the DLR',
     # long_description=long_description,  # TODO
@@ -16,10 +15,10 @@ setup(
     # keywords='',  # TODO
     author='Jonas Aumeier, Sebastian Baur, Joschka Herteux, Youssef Mabrouk',
     author_email='Jonas.Aumeier@dlr.de, Sebastian.Baur@dlr.de, Joschka.Herteux@dlr.de, Youssef.Mabrouk@dlr.de',
-    maintainer='Jonas.Aumeier, Sebastian.Baur, Joschka.Herteux, Youssef.Mabrouk',
-    maintainer_email='Jonas.Aumeier@dlr.de, Sebastian.Baur@dlr.de, Joschka.Herteux@dlr.de, Youssef.Mabrouk@dlr.de',
-    url='https://gitlab.dlr.de/rescom/reservoir-computing',
-    download_url='https://gitlab.dlr.de/rescom/reservoir-computing.git',
+    maintainer='Sebastian.Baur',
+    maintainer_email='Sebastian.Baur@dlr.de',
+    url='https://github.com/GLSRC/rescomp',
+    download_url='https://github.com/GLSRC/rescomp.git',
     # package_dir = {'': 'src'},
     # packages=['rescomp'],
     packages=setuptools.find_packages(),
